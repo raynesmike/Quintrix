12e7f4ce51defe8ec0b267fe00998bfa8cfdce22
Michael Raynes <raynesmike@yahoo.com>
2022-03-08 08:27:57 -0800

    hw5 fixed repeated codes

--- Homework/src/ButtonDumb.js
+++ Homework/src/ButtonDumb.js
@@ -3,68 +3,62 @@ import {Text, TouchableOpacity} from 'react-native';
 import React from 'react';
 
 const convertSize = (size) => {
+  let dynamicStyle ={}
   switch(size){
     case 'xxxxs': 
-    style.container = { 
-      ...style.container,
+    dynamicStyle = {
       paddingHorizontal: 1, 
       paddingVertical: 3
     }
     break;
     case 'xxxs': 
-    style.container = { 
-      ...style.container,
+    dynamicStyle = {
       paddingHorizontal: 2, 
       paddingVertical: 3
     }
     break;
     case 'xxs':    
-     style.container = {  
-      ...style.container,
+     dynamicStyle = { 
        paddingHorizontal: 2, 
        paddingVertical: 5
       }
       break;
     case 'xs':     
-    style.container = {  
-      ...style.container,
+    dynamicStyle = { 
       paddingHorizontal: 4, 
       paddingVertical: 8}
       break;
     case 's':     
-    style.container = {  
-      ...style.container,
+    dynamicStyle = { 
       paddingHorizontal: 7, 
       paddingVertical: 10}
 
       break;
     case 'md':     
-    style.container ={  
-      ...style.container,
+    dynamicStyle ={ 
       paddingHorizontal: 12, 
       paddingVertical: 15}
 
       break;
     case 'lg':    
-    style.container = {  
-      ...style.container,
+    dynamicStyle = { 
       paddingHorizontal: 30, 
       paddingVertical: 25}
 
       break;
     case 'lg':    
-      style.container = {  
-        ...style.container,
+      dynamicStyle = {  
         paddingHorizontal: 40, 
         paddingVertical: 30}
       break;
     default:    
-      style.container = {  
-        ...style.container,
+      dynamicStyle = {  
         paddingHorizontal: 25, 
         paddingVertical: 20}
       break
   }
+  style.container ={...style.container, ...dynamicStyle}
+  console.log(style.container)
 }
 
 const paddSizes = {
